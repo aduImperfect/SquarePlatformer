4ac63074094aa2d3a68d8c1cc38032eadc27f9a0
Aditya Subramanian <adu101992@gmail.com>
2026-07-02 16:56:02 +0530

    - Shifted to using CharacterBody instead of Rigidbody 2D and then found is_on_floor() call - Updated usage of values for up and horiz speeds to smoothen out the jump past a chasm.

--- square-platformer/TSCN/player_input_mgr.gd
+++ square-platformer/TSCN/player_input_mgr.gd
@@ -1,7 +1,9 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 #@export var forward_vector : Vector2 = global_transform.x
 #@export var up_vector : Vector2 = -global_transform.y
+
+@export var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @export var up_speed : float = 0.0
 @export var horiz_speed : float = 0.0
@@ -22,18 +24,18 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if no_input == true:
 		if horiz_speed < -0.1:
-			horiz_speed += _delta * 20.0
+			horiz_speed += _delta * 100.0
 		elif  horiz_speed > 0.1:
-			horiz_speed -= _delta * 20.0
+			horiz_speed -= _delta * 100.0
 		else:
 			horiz_speed = 0.0
 
 	if is_jumping == true && grounded == true:
-		up_speed = 300.0
+		up_speed = 500.0
 		grounded = false
 	elif grounded == false:
 		if up_speed > 0.1:
-			up_speed -= _delta * 200.0
+			up_speed -= _delta * 100.0
 		else:
 			up_speed = 0.0
 
@@ -43,12 +45,20 @@ func _physics_process(_delta: float) -> void:
 	position.x += _delta * horiz_speed
 	position.y -= _delta * up_speed
 
+	if not is_on_floor():
+		grounded = false
+		velocity.y += gravity * _delta
+	else:
+		grounded = true
+		up_speed = 0.0
+
+	move_and_slide()
 
 func _input(_event: InputEvent) -> void:
 	if _event is InputEventKey and _event.keycode == KEY_LEFT:
 		if _event.is_pressed():
 			#print("LEFT KEY PRESSED")
-			horiz_speed = -50.0
+			horiz_speed = -250.0
 			no_input = false
 		else:
 			no_input = true
@@ -56,7 +66,7 @@ func _input(_event: InputEvent) -> void:
 	if _event is InputEventKey and _event.keycode == KEY_RIGHT:
 		if _event.is_pressed():
 			#print("RIGHT KEY PRESSED")
-			horiz_speed = 50.0
+			horiz_speed = 250.0
 			no_input = false
 		else:
 			no_input = true
@@ -66,14 +76,3 @@ func _input(_event: InputEvent) -> void:
 			is_jumping = true
 		else:
 			is_jumping = false
-
-func _on_body_entered(_body: Node) -> void:
-	if _body.get_groups()[0] == "GROUND":
-		print("I AM ON THE GROUND AGAIN!")
-		grounded = true
-		up_speed = 0.0
-
-func _on_body_exited(_body: Node) -> void:
-	if _body.get_groups()[0] == "GROUND":
-		print("UP I GO!!")
-		grounded = false
