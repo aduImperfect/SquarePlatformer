5c23d791f4298179838b4c070d28bb1a12a24c24
William Cowan <william@modocosm.io>
2026-07-02 19:44:25 +0530

    Wall Jumps!

--- square-platformer/TSCN/player_input_mgr.gd
+++ square-platformer/TSCN/player_input_mgr.gd
@@ -22,6 +22,13 @@ extends CharacterBody2D
 @export var grounded : bool = true
 
 @export var playerStartPos : Vector2
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var WALL_SLIDE_SPEED = 100.0
+@export var WALL_JUMP_PUSHBACK = 400.0
+# --- Wall Jump Mechanics ---
+@export var wall_jump_lock_timer = 0.0
+@export var WALL_JUMP_LOCK_TIME = 0.10 # Time in seconds player control is locked
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -39,7 +46,7 @@ func _ready() -> void:
 	is_jumping = false
 	grounded = true
 	playerStartPos = position
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if no_input == true:
@@ -61,9 +68,38 @@ func _process(_delta: float) -> void:
 
 	_player_death()
 
+
 func _physics_process(_delta: float) -> void:
 	position.x += _delta * horiz_speed
 	position.y -= _delta * up_speed
+	
+	if not is_on_floor():
+		velocity.y += gravity * _delta
+
+	if wall_jump_lock_timer > 0:
+		wall_jump_lock_timer -= _delta
+
+	# 1. Handle Wall Sliding
+	if is_on_wall() and not is_on_floor() and velocity.y > 0:
+		velocity.y = min(velocity.y, WALL_SLIDE_SPEED)
+
+	# 2. Handle Jumping
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif is_on_wall() and not is_on_floor():
+			# Wall Jump: Use wall normal to push away
+			velocity.x = get_wall_normal().x * WALL_JUMP_PUSHBACK
+			velocity.y = JUMP_VELOCITY
+			wall_jump_lock_timer = WALL_JUMP_LOCK_TIME
+
+	# 3. Handle Horizontal Movement (with control lock)
+	if wall_jump_lock_timer <= 0:
+		var direction = Input.get_axis("ui_left", "ui_right")
+		velocity.x = direction * SPEED
+	else:
+		# Air control during wall jump lock (optional, keeps inertia)
+		velocity.x = move_toward(velocity.x, 0, 50)
 
 	if not is_on_floor():
 		grounded = false
