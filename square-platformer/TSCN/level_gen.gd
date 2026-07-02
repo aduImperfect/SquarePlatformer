5be75588d9761ce82a9488d374befdef18798a44
Aditya Subramanian <adu101992@gmail.com>
2026-07-02 18:08:13 +0530

    Level_1 separate scene instead of code gen of blocks!

--- square-platformer/TSCN/level_gen.gd
+++ square-platformer/TSCN/level_gen.gd
@@ -2,44 +2,34 @@ extends Node2D
 
 const PLAYER_SCENE = preload("res://TSCN/player.tscn")
 const BACKGROUND_SCENE = preload("res://TSCN/background.tscn")
-const GROUND_SCENE = preload("res://TSCN/ground.tscn")
-
-@export var groundMaxNum : int = 0
-@export var groundCount : int = 0
-@export var groundArr : Array[Node2D] = []
-
-@export var xBeginOffset : float = 0.0
-@export var yBeginOffset : float = 0.0
-@export var xOffset : float = 0.0
-@export var yOffset : float = 0.0
+const LEVEL_SCENE = preload("res://TSCN/level_1.tscn")
 
 @export var xBackCenter : float = 0.0
 @export var yBackCenter : float = 0.0
+
+@export var xLevel1Center : float = 0.0
+@export var yLevel1Center : float = 0.0
 
 @export var xPCenter : float = 0.0
 @export var yPCenter : float = 0.0
 
 @export var playerNode : Node2D
 @export var backgroundNode : Node2D
+@export var level1Node : Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	groundMaxNum = 40
-	groundCount = 0
-
-	xBeginOffset = 0.0
-	yBeginOffset = 600.0
-	xOffset = 40.0
-	yOffset = 0.0
-
 	xBackCenter = 550.0
 	yBackCenter = 350.0
+
+	xLevel1Center = 0.0
+	yLevel1Center = 0.0
 
 	xPCenter = 50.0
 	yPCenter = 550.0
 
 	_spawn_background()
-	_spawn_ground()
+	_spawn_level1()
 	_spawn_player()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -53,18 +43,12 @@ func _spawn_background() -> void:
 	add_child(background_instance)
 	backgroundNode = background_instance
 
-func _spawn_ground() -> void:
-	var ground_instance
-	for k in groundMaxNum:
-		if k >= 15 && k < 20:
-			continue
-
-		ground_instance = GROUND_SCENE.instantiate()
-		ground_instance.global_position.x = xBeginOffset + (k * xOffset)
-		ground_instance.global_position.y = yBeginOffset
-		add_child(ground_instance)
-		groundArr.append(ground_instance)
-		groundCount += 1
+func _spawn_level1() -> void:
+	var level1_instance = LEVEL_SCENE.instantiate()
+	level1_instance.global_position.x = xLevel1Center
+	level1_instance.global_position.y = yLevel1Center
+	add_child(level1_instance)
+	level1Node = level1_instance
 
 func _spawn_player() -> void:
 	var player_instance = PLAYER_SCENE.instantiate()
