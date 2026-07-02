aad8521f33bcbca7e9f8f43de1dbd29e12f15aed
Aditya Subramanian <adu101992@gmail.com>
2026-07-02 16:40:40 +0530

    Pushing changes in tied with jump and movement code. Still looking into why the collision signal is not giving any information for entering and exiting ground!

--- square-platformer/TSCN/player_input_mgr.gd
+++ square-platformer/TSCN/player_input_mgr.gd
@@ -1,17 +1,22 @@
 extends RigidBody2D
 
-@export var forward_vector : Vector2 = -global_transform.x
-@export var up_vector : Vector2 = -global_transform.y
+#@export var forward_vector : Vector2 = global_transform.x
+#@export var up_vector : Vector2 = -global_transform.y
 
 @export var up_speed : float = 0.0
 @export var horiz_speed : float = 0.0
 
 @export var no_input : bool = false
+@export var is_jumping : bool = false
+@export var grounded : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	up_speed = 20.0
+	up_speed = 0.0
 	horiz_speed = 0.0
+	no_input = false
+	is_jumping = false
+	grounded = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -22,10 +27,22 @@ func _process(_delta: float) -> void:
 			horiz_speed -= _delta * 20.0
 		else:
 			horiz_speed = 0.0
-	#print(horiz_speed)
+
+	if is_jumping == true && grounded == true:
+		up_speed = 300.0
+		grounded = false
+	elif grounded == false:
+		if up_speed > 0.1:
+			up_speed -= _delta * 200.0
+		else:
+			up_speed = 0.0
+
+	#print(up_speed)
 
 func _physics_process(_delta: float) -> void:
 	position.x += _delta * horiz_speed
+	position.y -= _delta * up_speed
+
 
 func _input(_event: InputEvent) -> void:
 	if _event is InputEventKey and _event.keycode == KEY_LEFT:
@@ -44,7 +61,19 @@ func _input(_event: InputEvent) -> void:
 		else:
 			no_input = true
 
-	#if _event is InputEventKey and _event.keycode == KEY_UP:
-		#if _event.is_pressed():
-			#print("UP KEY PRESSED")
-			#apply_force(up_vector * up_speed)
+	if _event is InputEventKey and _event.keycode == KEY_UP:
+		if _event.is_pressed():
+			is_jumping = true
+		else:
+			is_jumping = false
+
+func _on_body_entered(_body: Node) -> void:
+	if _body.get_groups()[0] == "GROUND":
+		print("I AM ON THE GROUND AGAIN!")
+		grounded = true
+		up_speed = 0.0
+
+func _on_body_exited(_body: Node) -> void:
+	if _body.get_groups()[0] == "GROUND":
+		print("UP I GO!!")
+		grounded = false
